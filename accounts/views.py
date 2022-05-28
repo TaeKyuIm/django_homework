5a116e69a122ec80b7b5e3e204248e734b937966
임태규 <kyu5787@naver.com>
2022-05-28 23:31:32 +0900

    django quiz 5.28

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 def login(request):
-    return
-
+    if request.method == 'POST':
+        username = request.POST['Username']
+        password = request.POST['Password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+    else:
+        return render(request, 'login.html')
 
 def logout(request):
-    return
+    auth.logout(request)
+    return redirect('home')
 
 def signup(request):
     return
